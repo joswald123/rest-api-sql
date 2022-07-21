ddc08ec2fba3b4c73c9aa0b1b0a3bbf7b64507b9
joswald123 <julianaandreaoswald@gmail.com>
2022-07-21 10:41:19 -0700

    Update user routes

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models').User;
-const { authenticateUser } = require('../middleware/auth-user');
+const { authenticateUser} = require('../middleware/auth-user');
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -17,8 +17,10 @@ function asyncHandler(cb){
 
 // Route that returns a list of users.
 router.get('/users', authenticateUser, asyncHandler(async(req, res) => {
-    const users = await User.findAll();
-    res.status(200).json(users);
+  const user = req.currentUser; 
+  await User.findAll();
+  res.status(200).json(user);
+    
 }));
 
 
@@ -26,7 +28,7 @@ router.get('/users', authenticateUser, asyncHandler(async(req, res) => {
 router.post('/users', asyncHandler(async (req, res) => {
     try {
         const user = await User.create(req.body);
-        res.status(201).location('/').json(user).end();
+        res.status(201).location('/courses/' + req.params.id ).end();
       } catch (error) {
         if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
           const errors = error.errors.map(err => err.message);
